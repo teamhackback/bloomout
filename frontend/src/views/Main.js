2c8aacf8736ca377ff63cdad8c19f1de4a01c61d
Dalimil Hajek <dalimilhajek@gmail.com>
2017-04-08 10:07:04 +0100

    Add welcome-left-screen view

--- frontend/src/views/Main.js
+++ frontend/src/views/Main.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-
 import Styles from './Main.css';
 
 class SplitLayout extends Component {
@@ -17,9 +16,47 @@ class SplitLayout extends Component {
   }
 }
 
-const Intro = () => (
-  <SplitLayout left="the" right={"the right"} />
+const WelcomeLeftScreen = (props) => (
+<div className="WelcomeLeftScreen">
+  <h1>Welcome to your company</h1>
+  <p>Discover what risks you have in your management and employee relations.
+    Pop-up your official trust fund polaroid, put a bird on it stumptown enamel semantics actually.
+  </p>
+  <button onClick={props.showOverview}>Show me more</button>
+</div>
 );
+
+const WelcomeOverviewDetailed = () => (
+  <div>detailed overview left</div>
+);
+const WelcomeOverviewBasic = () => (
+  <div>basic overview right</div>
+);
+
+class Intro extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      overviewShown: false
+    };
+    this.showOverview = this.showOverview.bind(this);
+  }
+
+  showOverview() {
+    this.setState({ overviewShown: true });
+  }
+
+  render() {
+    return (
+      <SplitLayout
+        left={ this.state.overviewShown ?
+          <WelcomeOverviewDetailed /> : <WelcomeLeftScreen showOverview={this.showOverview}/>
+        }
+        right={<WelcomeOverviewBasic />}
+      />
+    );
+  }
+} 
 
 class Main extends Component {
   render() {
