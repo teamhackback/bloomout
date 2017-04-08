e9a22a39f781cfe294860c92cd703aa550dafbfb
Jait Dixit <jait.dixit@tum.de>
2017-04-08 12:35:10 +0200

    Add message generation to generate_db and deploy_watch fix

--- backend/deploy_watch.sh
+++ backend/deploy_watch.sh
@@ -2,4 +2,5 @@
 
 host=${HOST:-wehstyle.hackback.tech}
 
-watch -n1 rsync -avr --exclude venv --exclude __pycache__ backend/ root@${host}:/leap/backend
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+watch -n1 rsync -avr --exclude venv --exclude __pycache__ ${DIR}/ root@${host}:/leap/backend
