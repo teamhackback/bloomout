b2377ea4803a32a4860aa060c799b9237a3fa4d3
Dalimil Hajek <dalimilhajek@gmail.com>
2017-04-08 11:28:53 +0100

    Add animate.css

--- frontend/src/views/Main.js
+++ frontend/src/views/Main.js
@@ -16,12 +16,30 @@ class SplitLayout extends Component {
   }
 }
 
+class Tiles extends Component {
+  render() {
+    const colors = ['#A4A4A4', "#727272", "#B7B7B7", "#918E8E", "#606060", "#4D4D4D"];
+    return (
+      <div className="tiles">
+        {this.props.tiles.map((tile, ind) => (
+          <div key={ind} className={`tile`}
+            style={{
+              background: colors[ind]
+            }}>
+            <span className="title">{tile.name}</span>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
 const Button = (props) => (
   <a className="button" href="#" {...props}>{props.children}<span></span></a>
 );
 
 const WelcomeLeftScreen = (props) => (
-<div className="WelcomeLeftScreen">
+<div className="WelcomeLeftScreen animated fadeIn">
   <div>logo</div>
   <h1>Welcome, <span className="c-p">hackBack</span></h1>
   <strong>This is your risk management application</strong>
@@ -34,8 +52,27 @@ const WelcomeLeftScreen = (props) => (
 );
 
 const WelcomeOverviewDetailed = () => (
-  <div className="WelcomeOverviewDetailed">
-    detailed overview left
+  <div className="WelcomeOverviewDetailed animated fadeIn">
+    <Tiles tiles={[
+      {
+        name: 'Employee risks'
+      },
+      {
+        name: 'Project risks'
+      },
+      {
+        name: 'Client risks'
+      },
+      {
+        name: 'Quality of interactions'
+      },
+      {
+        name: 'Recommendations'
+      },
+      {
+        name: 'Black box score'
+      }
+    ]} />
   </div>
 );
 const WelcomeOverviewBasic = () => (
@@ -60,7 +97,7 @@ class Intro extends Component {
   render() {
     return (
       <SplitLayout
-        left={ this.state.overviewShown ?
+        left={ !this.state.overviewShown ?
           <WelcomeOverviewDetailed /> : <WelcomeLeftScreen showOverview={this.showOverview}/>
         }
         right={<WelcomeOverviewBasic />}
