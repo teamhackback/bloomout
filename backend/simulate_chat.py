565b8eb6a51183d2be563c4bacfec780faa7062b
Jait Dixit <jait.dixit@tum.de>
2017-04-08 22:07:28 +0200

    insert cached messages instead of new messages

--- backend/simulate_chat.py
+++ backend/simulate_chat.py
@@ -1,21 +1,29 @@
 import sys
+import json
 import time
-import random
-from generate_db import generate_message
 from mongo_base import messages
 
+
+saved_messages = None
+
+with open('./message_dump', 'r') as f:
+    contents = f.read()
+    saved_messages = json.loads(contents)
+
+print('dropping previous messages')
+messages.drop()
+
 try:
-    used_files = []
     counter = 0
     while True:
-        file_index = random.randint(0, 25000)
-        while file_index in used_files:
-            file_index = random.randint(0, 25000)
-        messages.insert_one(generate_message(counter, file_index))
+        message = saved_messages[counter % len(saved_messages)]
+        if '_id' in message:
+            del message['_id']
+        message['id'] = counter
+        messages.insert_one(message)
+        print('inserted message: ', counter)
 
-        used_files.append(file_index)
         counter += 1
-
         time.sleep(2)
 except KeyboardInterrupt:
     sys.exit()
