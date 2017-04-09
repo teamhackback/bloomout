8a05c3e987c1896a8d4ef06ee5095d0016a86eb6
Sebastian Wilzbach <seb@wilzba.ch>
2017-04-09 12:50:14 +0200

    hide menu on page open

--- frontend/src/views/Sidebar.js
+++ frontend/src/views/Sidebar.js
@@ -4,24 +4,32 @@ import {slide as Menu} from 'react-burger-menu';
 import Images from '../assets';
 
 class Sidebar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: false
+    };
+  }
+  handleMenuClick = () => {
+    this.setState({isOpen: false});
+  };
   render() {
     return (
       <div className="bgMenu dark-theme">
-        <Menu>
+        <Menu isOpen={this.state.isOpen}>
           <div style={{
             display: "flex",
             flexDirection: "column",
-            
           }}>
             <div>
               <img src={Images.logoSide} role="presentation" style={{ margin: "1.2em 0.5em 2em"}}/>
             </div>
             <div className="side-navlist">
-              <div><LinkTo left="/overview/intro" right="/overview/basic">Overall risks</LinkTo></div>
-              <div><LinkTo left="/overview/basic" right="/overview/detailed">Risks per project</LinkTo></div>
-              <div><LinkTo left="/employees/basic" right="/employees/detailed">Employee risks</LinkTo></div>
-              <div><LinkTo left="/people/basic" right="/people/detailed">Engagement</LinkTo></div>
-              <div><LinkTo left="/overview/intro" right="/overview/basic">Interactions</LinkTo></div>
+              <div><LinkTo onClick={this.handleMenuClick} left="/overview/intro" right="/overview/basic">Overall risks</LinkTo></div>
+              <div><LinkTo onClick={this.handleMenuClick} left="/overview/basic" right="/overview/detailed">Risks per project</LinkTo></div>
+              <div><LinkTo onClick={this.handleMenuClick} left="/employees/basic" right="/employees/detailed">Employee risks</LinkTo></div>
+              <div><LinkTo onClick={this.handleMenuClick} left="/people/basic" right="/people/detailed">Engagement</LinkTo></div>
+              <div><LinkTo onClick={this.handleMenuClick} left="/overview/intro" right="/overview/basic">Interactions</LinkTo></div>
             </div>
             <footer style={{
               position: "absolute",
