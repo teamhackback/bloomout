05948c8fe70c00bedd676f39c65ecf435071f1e6
Sebastian Wilzbach <seb@wilzba.ch>
2017-04-09 10:21:23 +0200

    History API: color emotions

--- frontend/src/views/DebugHistoryItem.js
+++ frontend/src/views/DebugHistoryItem.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import personStore from '../PersonStore';
 import {observer} from "mobx-react";
+import {sortBy, maxBy, values} from 'lodash';
 
 
 function replaceAll(str, from, to) {
@@ -35,10 +36,54 @@ function Sender(props) {
   )
 }
 
+function ColoredTextBox(props) {
+  const part = props.item;
+  const styles = {};
+  if (part.type === "text") {
+  } else {
+    styles["fontWeight"] = "bold";
+    if (part.type === "joy") {
+      styles["color"] = "green";
+    } else {
+      styles["color"] = "red";
+    }
+  }
+  return (
+    <span style={styles} key={part.id}>{part.text}</span>
+  );
+}
+
 @observer
 export default class DebugHistoryItem extends Component {
   render() {
   const item = this.props.item;
+  const body = replaceAll(item.body, "<br />", "");
+  let keywords = item.emotion.keywords.map((k) => {
+    k.index = -1;
+    if (k.text !== "br")
+      k.index = body.search(new RegExp(k.text, "i"));
+    return k;
+  });
+  keywords = sortBy(keywords, "index");
+  const parts = [];
+  let lastIndex = 0;
+  keywords.forEach((k) => {
+    const index = k.index;
+    if (index >= 0) {
+      if (index < lastIndex)
+        return;
+      if (index > 0) {
+        parts.push({id: parts.length, text: body.slice(lastIndex, index), type: "text"});
+      }
+      if (k.emotion !== undefined) {
+        const maxEmotion = maxBy(Object.keys(k.emotion), e => k.emotion[e]);
+        parts.push({id: parts.length, text: body.slice(index, index + k.text.length), type: maxEmotion});
+        lastIndex = index + k.text.length;
+      }
+    }
+  });
+  if (lastIndex !== body.length - 1)
+    parts.push({id: parts.length, text: body.slice(lastIndex), type: "text"});
   return (
     <div style={{"margin": 10}}>
 
@@ -75,7 +120,7 @@ export default class DebugHistoryItem extends Component {
       </div>
 
       <div className="debug-msg">
-        { replaceAll(item.body, "<br />", "") }
+        {parts.map(part => <ColoredTextBox item={part} key={part.id} />)}
       </div>
     </div>
   )}
